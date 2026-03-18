d57d05be4c9e6d1610ff7dd32de1a1a7aaa26193
xauhoquadi123-art <xauhoquadi123@gmail.com>
2026-03-18 08:30:23 +0700

    add baitap3.js

---
+++ baitap3.js
@@ -0,0 +1,12 @@
+function tinhtoan(pheptinh) {
+let thunhat = parseFloat(document.getElementById("thunhat").value);
+let thuhai = parseFloat(document.getElementById("thuhai").value);
+if (isNaN(thunhat) || isNaN(thuhai))
+{document.getElementById('ketqua').innerText = "Vui lòng nhập cả hai số!";
+document.getElementById('ketqua').style.color = "orange";
+return}
+let ketqua = 0
+if (pheptinh === "cong"){ ketqua = thunhat + thuhai;}
+else if ( pheptinh === "tru"){ ketqua = thunhat - thuhai;}
+document.getElementById('ketqua').innerText = ketqua;
+document.getElementById('ketqua').style.color = "red";}
